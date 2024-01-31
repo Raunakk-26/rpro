58902e95d818a03cf9867cdb30f268551ea473f4
Raunak Das <62735203+Raunakk-26@users.noreply.github.com>
2024-01-31 19:01:12 +0530

    Add files via upload

---
+++ rpro.R
@@ -0,0 +1,18 @@
+{
+  d1=data.frame()
+  d2=edit(d1)
+  class(d2)
+}
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
